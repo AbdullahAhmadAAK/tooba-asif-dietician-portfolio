3235b11b9c63d56b16292d314a84e4a707ec343b
AbdullahAhmadAAK <abdullahaak06@gmail.com>
2024-12-01 19:37:15 +0500

    make hero section responsive 99%

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
   import HeaderSection from "./sections/HeaderSection.svelte";
+  import HeroSection from "./sections/HeroSection.svelte";
 </script>
-<HeaderSection />
 
+<HeaderSection />
+<HeroSection />
---
+++ src/routes/sections/HeroSection.svelte
@@ -0,0 +1,31 @@
+<div class="flex md:flex-row flex-col md:gap-12">
+
+  <img 
+    src="hero/tooba-asif-dietician-nutritionist.png" alt=""
+    class="aspect-square md:w-[40%] md:rounded-br-3xl md:rounded-bl-[0] rounded-b-3xl"
+  >
+
+  <div class="flex flex-col justify-center md:items-start items-center gap-4 my-4">
+    <h4 class="md:text-start text-center lg:text-[28px] md:text-[22px] text-[18px] text-[#1A3129] font-semibold border-[3px] border-b-[#D0D26E] border-x-0 border-t-0 w-fit">Transform Your ❤️ Health with</h4>
+    <h1 class="md:text-start text-center lg:text-[58px] md:text-[48px] text-[38px] text-[#1A3129] font-bold">Dn. Tooba Asif</h1>
+    <p class="md:text-start text-center lg:text-[18px] md:text-[16px] text-[14px] text-[#333333] max-w-[80%] font-medium">Welcome to Nutritionist, your partner in achieving optimal health through personalized nutrition coaching. I am here to guide you on your weight loss journey, providing customized plans and ongoing support. Start your transformation today and experience the power of personalized nutrition coaching.</p>
+  
+    <div class="flex flex-row mt-8 gap-5">
+      <a href="#contact-us">
+        <button class="rounded-lg lg:text-[18px] text-[14px] font-semibold text-[#262626] bg-[#CBEA7B] py-4 px-5">Get Started Today</button>
+      </a>
+
+      <a href="#contact-us">
+        <button class="rounded-lg lg:text-[18px] text-[14px] font-semibold text-[#262626] border border-[#E5F5BD] py-4 px-5">Book a Demo</button>
+      </a>
+    </div>
+
+    <div class="md:flex hidden flex-row gap-2 mt-6 items-center">
+      <img src="hero/three-girls-collage.png" class="lg:w-[130px] lg:h-[66px] md:w-[102px] md:h-[52px]" alt="">
+      <p class="text-[20px] font-semibold">
+        <span class="text-[#468671] text-bold">430+</span>
+        Happy Customers
+      </p>
+    </div>
+  </div>
+</div>
